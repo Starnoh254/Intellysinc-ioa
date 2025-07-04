ef5338fc690186eaf56959af4b127234cad9f88b
Alphadoane <superalphablood@gmail.com>
2025-07-04 17:31:46 +0300

    Move Solutions link to Company section in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,10 +11,13 @@ const Footer = () => {
         <div className="footer-column">
           <h4>Company</h4>
           <ul>
-            <li><Link to="/About">About Us</Link></li>
-            <li><Link to="/Team">Our Team</Link></li>
+            <li><Link to="/Home">Home</Link></li>
+            <li><Link to="/About">About</Link></li>
+            <li><Link to="/Team">Team</Link></li>
             <li><Link to="/Awards">Awards</Link></li>
+            <li><Link to="/CompanyInfo">Company Info</Link></li>
             <li><Link to="/Contact">Contact</Link></li>
+            <li><Link to="/Solutions">Solutions</Link></li>
           </ul>
         </div>
 
@@ -23,8 +26,14 @@ const Footer = () => {
           <ul>
             <li><Link to="/BusinessIntelligence">Business Intelligence</Link></li>
             <li><Link to="/DataAutomation">Data Automation</Link></li>
+            <li><Link to="/DataProcessing">Data Processing</Link></li>
             <li><Link to="/DocumentManagement">Document Management</Link></li>
             <li><Link to="/Integrations">Integrations</Link></li>
+            <li><Link to="/WorkflowAutomation">Workflow Automation</Link></li>
+            <li><Link to="/QualityManagement">Quality Management</Link></li>
+            <li><Link to="/SalesMarketing">Sales Marketing</Link></li>
+            <li><Link to="/MobileSolutions">Mobile Solutions</Link></li>
+            <li><Link to="/SecurityServices">Security Services</Link></li>
           </ul>
         </div>
 
@@ -35,6 +44,7 @@ const Footer = () => {
             <li><Link to="/CaseStudies">Case Studies</Link></li>
             <li><Link to="/Resources">Resources</Link></li>
             <li><Link to="/Support">Support</Link></li>
+            <li><Link to="/Testimonials">Testimonials</Link></li>
           </ul>
         </div>
 
